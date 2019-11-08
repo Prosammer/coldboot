56a6aa227278961fab7b63214c4a7179fe8be75f
Sam Finton <spamhinting@gmail.com>
2019-11-08 14:01:55 -0400

    Added footer template

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@ module.exports = {
   theme: {
     extend: {
         colors: {
-            'map-green': '#E2ECD4'
+            'map-green': '#E2ECD4',
+            'footer-blue': '#3F4B3B'
         }
     }
   },
