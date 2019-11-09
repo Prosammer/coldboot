5801e5ea8cd868f5f36a5318aafd9c6dee524a47
Sam Finton <spamhinting@gmail.com>
2019-11-08 20:03:40 -0400

    Working footer (no content)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,34 @@
 module.exports = {
-  theme: {
-    extend: {
-        colors: {
-            'map-green': '#E2ECD4',
-            'footer-blue': '#3F4B3B'
+    theme: {
+        extend: {
+            colors: {
+                'map-green': '#E2ECD4',
+                'footer-blue': '#3F4B3B',
+                'brand_blue': {
+                    100: '#EFF7FD',
+                    200: '#D8ECFB',
+                    300: '#C0E0F9',
+                    400: '#91C9F4',
+                    500: '#62B2EF',
+                    600: '#58A0D7',
+                    700: '#3B6B8F',
+                    800: '#2C506C',
+                    900: '#1D3548',
+                },
+                'brand_orange': {
+                    100: '#FFF8F0',
+                    200: '#FFEEDA',
+                    300: '#FFE3C3',
+                    400: '#FECE96',
+                    500: '#FEB969',
+                    600: '#E5A75F',
+                    700: '#986F3F',
+                    800: '#72532F',
+                    900: '#4C3820',
+                },
+            }
         }
-    }
-  },
-  variants: {},
-  plugins: []
+    },
+    variants: {},
+    plugins: []
 }
